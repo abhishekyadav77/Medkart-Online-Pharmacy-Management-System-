570efcf078653865ee887c5f2acfce5da2ce5927
abhishekyadav77 <yadavvikky012@gmail.com>
2026-09-19 21:26:02 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -39,4 +39,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen("0.0.0.0", PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
